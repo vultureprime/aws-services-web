645fdb0a266a91d3c82948cc68dffbc474bf1fa9
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-07-07 21:16:50 +0700

    add case study

--- src/app/features/Home/CaseStudy.tsx
+++ src/app/features/Home/CaseStudy.tsx
@@ -2,8 +2,35 @@
 import Image from 'next/image'
 import { MotionViewport, varFade } from '../../animate'
 import { m } from 'framer-motion'
+import Link from 'next/link'
 
 export default function Home() {
+  const caseStudy = [
+    {
+      title: 'ImageXYZ',
+      tool: 'EC2 • S3 • NextJs • Python',
+      blog: 'https://www.vultureprime.com/blogs/imagexyz-tech-stack',
+      website: 'https://imagexyz.vultureprime.com/',
+      img: '/images/case-1.png',
+      desc: ' Website สำหรับการ Resize รูปภาพจาก format ต่างๆให้เป็น Webp format เพื่อให้เหมาะแก่การพัฒนา web application',
+    },
+    {
+      title: 'LaiTa ',
+      tool: 'EC2 • S3 • NextJs • Python',
+      blog: 'https://www.vultureprime.com/blogs/laita-the-series',
+      website: 'https://laita.co/',
+      img: '/images/case-2.png',
+      desc: 'service สำหรับการทำ face search คือการค้นหารูปตัวเองจาก Album โดยการอัพโหลดรูปตัวเองขึ้นไปในระบบ',
+    },
+    {
+      title: 'OgXYZ',
+      tool: 'EC2 • S3 • NextJs • Nodejs',
+      blog: 'https://www.vultureprime.com/blogs/ogxyz-the-series',
+      website: 'https://ogxyz.vultureprime.com/',
+      img: '/images/case-3.png',
+      desc: 'OgXYZ เป็น Service ที่สร้างมาสำหรับการสร้าง Open Graph ให้กับ website',
+    },
+  ]
   return (
     <div className='px-6 lg:px-10 py-20 max-w-7xl  mx-auto'>
       <MotionViewport>
@@ -25,35 +52,75 @@ export default function Home() {
               <span className='text-cornflower-500 font-medium'>
                 Vulture Prime The Series
               </span>{' '}
-              เรามุ่งเน้นในการให้บริการที่มีประสิทธิภาพ ความน่าเชื่อถือ
-              และความน่าเป็นมืออาชีพที่สุด
+              <br /> Get start with AWS Service
             </p>
           </div>
         </m.div>
       </MotionViewport>
       <MotionViewport className='flex items-center justify-center'>
-        <div className='mt-8 flex flex-wrap items-center justify-center gap-8'>
-          <Image
-            src='/images/cer-1.png'
-            alt='cer-1'
-            width={206}
-            height={284}
-            priority
-          />
-          <Image
-            src='/images/cer-2.png'
-            alt='cer-2'
-            width={206}
-            height={284}
-            priority
-          />
-          <Image
-            src='/images/cer-3.png'
-            alt='cer-3'
-            width={206}
-            height={284}
-            priority
-          />
+        <div className='mt-8 grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8'>
+          {caseStudy.map((item, index) => (
+            <div
+              key={index}
+              className='group bg-cornflower-50 rounded-xl overflow-hidden shadow-md hover:scale-105 transition-all duration-300'
+            >
+              <div className='w-full h-full relative rounded-t-xl overflow-hidden'>
+                <Image
+                  src={item.img}
+                  alt='case-1'
+                  width={0}
+                  height={0}
+                  sizes='100vw'
+                  className='group-hover:scale-105 transition-all duration-300'
+                  style={{ width: '100%', height: 'auto' }} // optional
+                  priority
+                />
+                <div className='p-4 flex flex-col bg-[linear-gradient(360deg,rgba(1,1,1,0.27)_87.01%,rgba(1,1,1,0.01)_100%)] absolute bottom-0 left-0 w-full h-[80px] text-blue-50'>
+                  <p className='text-xs lg:text-sm mt-auto'>{item.tool}</p>
+                </div>
+              </div>
+              <div className='p-4 h-[160px]'>
+                <p className='text-md lg:text-lg font-medium text-cornflower-800'>
+                  {item.title}
+                </p>
+                <p className='text-xs text-sky-700 line-clamp-2'>{item.desc}</p>
+                <div className='flex items-center gap-x-2 mt-4'>
+                  <Link
+                    className='hover:scale-110 transition-all duration-300 flex gap-x-2 text-sm items-center text-cornflower-800'
+                    href={item.website}
+                    title={item.title}
+                  >
+                    <Image
+                      src='/images/website.svg'
+                      alt='web-1'
+                      width={20}
+                      height={20}
+                      sizes='100vw'
+                      priority
+                    />
+                    <p>ลองเล่นเลย</p>
+                  </Link>
+                </div>
+                <div className='flex items-center gap-x-2 mt-4'>
+                  <Link
+                    className='hover:scale-110 transition-all duration-300 flex gap-x-2 text-sm items-center text-cornflower-800'
+                    href={item.blog}
+                    title={item.title}
+                  >
+                    <Image
+                      src='/images/content.svg'
+                      alt='content-1'
+                      width={20}
+                      height={20}
+                      sizes='100vw'
+                      priority
+                    />
+                    <p>อ่านบทความ</p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </MotionViewport>
     </div>
