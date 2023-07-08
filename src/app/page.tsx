5e9da2835499efd2de5e7466c06252c4a59d932b
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-07-08 12:57:06 +0700

    remove don't used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 'use client'
-import Image from 'next/image'
-import { MotionViewport, varFade } from './animate'
-import { m } from 'framer-motion'
 import Hero from './features/Home/Hero'
 import AWSCapabilities from './features/Home/AWSCapabilities'
 import WhyUs from './features/Home/WhyUs'
